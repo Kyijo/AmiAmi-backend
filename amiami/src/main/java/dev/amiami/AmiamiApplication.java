3b0f6b0f27364019d4705a8ae3f9b3c77fda28f4
Kyijo <dominikborufka@seznam.cz>
2023-05-23 17:59:01 +0200

    Initial commit

---
+++ amiami/src/main/java/dev/amiami/AmiamiApplication.java
@@ -0,0 +1,13 @@
+package dev.amiami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiamiApplication.class, args);
+	}
+
+}
---
+++ amiami/src/test/java/dev/amiami/AmiamiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amiami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
